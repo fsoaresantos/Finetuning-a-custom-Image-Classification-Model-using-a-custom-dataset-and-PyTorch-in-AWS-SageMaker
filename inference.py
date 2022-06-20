dcee7d3744268a30a4929be8dddf85e065f0bd2c
fsoaresantos <80052315+fsoaresantos@users.noreply.github.com>
2022-06-20 20:56:49 +0200

    Add files via upload

---
+++ inference.py
@@ -0,0 +1,296 @@
+#TODO: Import your dependencies.
+#For instance, below are some dependencies you might need if you are using Pytorch
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.models as models
+from torchvision import datasets, transforms
+
+#parser for reading command-line arguments
+import argparse
+import time
+import os
+import shutil
+import json
+
+
+def test(model, test_loader, criterion, device):
+    '''
+        This function takes a model and a testing data loader and get the
+        test accuray/loss of the model.
+    '''
+    # set model to evaluate mode
+    model.eval() #because finetuning (pre-trained)
+    
+    correction = 0
+    loss_counter = 0
+    
+    with torch.no_grad():
+        for image, target in test_loader:
+            image = image.to(device)
+            target = target.to(device)
+            
+            output = model(image)
+            loss = criterion(output, target)
+            pred = torch.argmax(input = output, dim = 1)
+            
+            loss_counter += loss.item()
+            correction += torch.sum(pred == target.data).item()
+            
+    # print statistics
+    print(f"Test set: Average loss: {loss_counter/len(test_loader.dataset)}")
+    print(f"Test Accuracy {correction/len(test_loader.dataset)*100}%\n")
+
+
+
+def train(model, train_loader, criterion, optimizer, device):
+    '''
+        This function takes a model and data loaders for training 
+        and will get train the model.
+    '''
+    
+    loss_counter = 0
+    
+    # set model to training mode
+    model.train()
+
+    # training loop
+    for batch_n, (image, target) in enumerate(train_loader, 1):
+        image = image.to(device)
+        target = target.to(device)
+        
+        optimizer.zero_grad()
+        output = model(image)
+        loss = criterion(output, target)
+        loss.backward()
+        optimizer.step()
+
+        #statistics
+        loss_counter += loss.item()
+        
+    # return loss
+    return loss_counter/len(train_loader.dataset)
+
+
+
+def net():
+    '''
+        This function initializes the model.
+        Remember to use a pretrained model
+    '''
+
+    # download pretrained model
+    model = models.resnet18(pretrained = True)
+    
+    # freeze all the pre-trained layers
+    for p in model.parameters():
+        p.requires_grad = False
+    
+    # get the numbers of input features in the last FC layer
+    nin_features = model.fc.in_features
+    
+    # replace the last FC layer to meet the number of classes (out_features) in the actual dataset
+    model.fc = nn.Sequential(nn.Linear(in_features = nin_features, out_features = 133)) #oss. there're 133 n_classes in the dataset
+    
+    return model
+
+
+
+def create_data_loaders(data_path, batch_size):
+    '''
+    This is an optional function that you may or may not need
+    depending on whether you need to use data loaders or not
+    '''
+
+    transform = transforms.Compose([
+        transforms.Resize([224, 224]),
+        transforms.ToTensor(),
+        transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+    ])
+
+    dataset = datasets.ImageFolder(root=data_path, transform=transform)
+
+    loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size)
+    
+    return loader
+
+
+
+def save_model(model, path_to_model):
+    ## Save the trained model to 'SM_MODEL_DIR' /opt/ml/model
+    print('Saving the model!!!')
+
+    modeldir_path = f"{path_to_model}/modelp3.pth" #os.path.join(path_to_model, "modelp3.pth") ::::::!!!!!!!
+    with open(modeldir_path, 'wb') as f:
+        torch.save(model.state_dict(), f)
+    
+    # PyTorch requires the inference script to be in a folder called code inside of model directory
+    inference_path = f"{path_to_model}/code/" #os.path.join(path_to_model, 'code/') ::::::!!!!!!!
+
+    if not os.path.exists(inference_path):
+        os.mkdir(inference_path)
+
+    shutil.copy("inference.py", inference_path)
+
+    print(f"Saving inference code to {inference_path}")
+    print(f"Model saved to {modeldir_path}")
+
+
+
+def main(args):
+    
+    # define device to run the model
+    use_cuda = args.num_gpus > 0
+    print(f"Number of gpus available - {args.num_gpus}")
+    device = torch.device("cuda" if use_cuda else "cpu")
+    print(f"Running on device {device}")
+    #device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    #print(f"Running on device {device}")
+    
+    ## Initialize the model by calling the net function
+    model = net()
+    
+    # move model to GPU memory
+    model = model.to(device)
+    
+    ## Create your loss and optimizer
+    loss_criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr = args.lr)
+    
+    ## Call the train function to start training your model
+    # create_data_loaders load data from S3
+    train_loader = create_data_loaders(args.data_train, args.batch_size)
+    for epoch in range(1, args.epochs + 1):
+        loss = train(model, train_loader, loss_criterion, optimizer, device)
+        # print out training progress
+        print("Train Epoch: {}, Average Loss: {}".format(epoch, loss))
+
+    ## Test the model to see its accuracy
+    test_loader = create_data_loaders(args.data_test, args.test_batch_size) 
+    test(model, test_loader, loss_criterion, device)
+
+    save_model(model, args.model_dir)
+    
+
+
+def model_fn(model_dir):
+    """
+    Load/deserialize the model and return it for 
+    
+    Keyword arguments:
+    model_dir -- the directory path to model artifacts
+    """
+
+    model = net()
+    
+    modeldir_path = f"{path_to_model}/modelp3.pth" #os.path.join(model_dir, "modelp3.pth") ::::::!!!!!!!
+    with open(modeldir_path, 'rb') as f:
+        model.load_state_dict(torch.load(f))
+
+    return model
+    
+
+
+def input_fn(request_body, request_content_type):
+    """
+    Deseriaize request_body into a object that can be used for prediction
+    request_body is the s3 path to image(s)
+    """
+    
+    transform = transforms.Compose([
+        transforms.Resize([224, 224]),
+        transforms.ToTensor(),
+        transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+    ])
+
+    if request_content_type == "application/x-image":
+        dataset = datasets.ImageFolder(root=request_body, transform=transform)
+        return(torch.utils.data.DataLoader(dataset, batch_size=1))
+        #return(create_data_loaders(request_body, 1))
+
+
+
+def predict_fn(input_object, model):
+    """
+    Apply the model to the deserialized object
+    input_object is the object returned by input_fn
+    model is the model loaded by model_fn
+    """
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = model.to(device)
+    model.eval()
+
+    with torch.no_grad():
+        for image, target in input_object:
+            image = image.to(device)
+            target = target.to(device)
+            output = model(image)
+            
+    return output.numpy()
+
+    
+
+if __name__=='__main__':
+    
+    parser=argparse.ArgumentParser(description = "define hyperparameters for fine tuning Pytorch CIFAR10 model")
+    
+    ## Specify training args (hyperparameters sent by user)
+    parser.add_argument(
+        "--batch_size",
+        type=int,
+        default=40,
+        metavar="N",
+        help="training batch size (default: 64)"
+    )
+    
+    parser.add_argument(
+        "--test_batch_size",
+        type=int,
+        default=209,
+        metavar="N",
+        help="test batch size (default: 100)"
+    )
+    
+    parser.add_argument(
+        "--epochs",
+        type=int,
+        default=5,
+        metavar="N",
+        help="number of epochs to train (default: 10)"
+    )
+    
+    parser.add_argument(
+        "--lr",
+        type=float,
+        default=0.003,
+        metavar="LR",
+        help="learning rate (default: 3e-2)"
+    )
+    
+    # get container environment variables
+    parser.add_argument("--hosts", type=list, default=json.loads(os.environ["SM_HOSTS"]))
+    parser.add_argument("--current_host", type=str, default=os.environ["SM_CURRENT_HOST"])
+    parser.add_argument("--num_gpus", type=int, default=os.environ["SM_NUM_GPUS"]) ## number of gpus in the current container
+    
+    # Model directory (the default set by SageMaker, /opt/ml/model)
+    #parser.add_argument("--model_dir", type=str, default=os.environ.get("SM_MODEL_DIR"))
+    parser.add_argument("--model_dir", type=str, default=os.environ["SM_MODEL_DIR"]) ## path to model artifacts
+    
+    # Data directories
+    parser.add_argument('--channel_names', default=json.loads(os.environ['SM_CHANNELS'])) ## names of data channels
+    parser.add_argument("--data_train", type=str, default=os.environ["SM_CHANNEL_TRAIN"]) ## path to training data
+    parser.add_argument('--data_test', type=str, default=os.environ['SM_CHANNEL_TEST']) ## path to test data
+    parser.add_argument('--data_valid', type=str, default=os.environ['SM_CHANNEL_VALID']) ## path to validation data
+    parser.add_argument("--data_train_lst", type=str, default=os.environ["SM_CHANNEL_TRAIN_LST"]) ## path to training metadata
+    parser.add_argument('--data_test_lst', type=str, default=os.environ['SM_CHANNEL_TEST_LST']) ## path to test metadata
+    parser.add_argument('--data_valid_lst', type=str, default=os.environ['SM_CHANNEL_VALID_LST']) ## path to validation metadata
+   
+    # end argument definition
+    args=parser.parse_args()
+    
+    main(args)
+
+    
